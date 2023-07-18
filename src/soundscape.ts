9c74806e4b5b95911c3d4c2afd03b9a204a54124
Luke Escobar <lukeeescobar@gmail.com>
2023-07-17 20:24:49 -0400

    soundscape audio edits

--- src/soundscape.ts
+++ src/soundscape.ts
@@ -11,12 +11,12 @@ voiceover.playing = true
 voiceover.loop = true
 voiceover.volume = 1
 
-const sound03 = new AudioClip("sounds/MESH_ENV_mixdown_STOP.mp3")
+const sound03 = new AudioClip("sounds/pond.mp3")
 const pond = new AudioSource(sound03)
 
 pond.playing = true
 pond.loop = true
-pond.volume = .5
+pond.volume = .3
 
 //voice over audio
 const box1 = new Entity()
@@ -25,8 +25,8 @@ box1.getComponent(BoxShape).withCollisions = false
 box1.getComponent(BoxShape).visible = false
 box1.addComponent(voiceover)
 box1.addComponent(new Transform({
-        position: new Vector3(32, 1, 160),
-        scale: new Vector3(0, 0, 0)
+        position: new Vector3(32, -10, 160),
+        scale: new Vector3(1, 1, 1)
 }))
 engine.addEntity(box1)
 
@@ -34,15 +34,15 @@ engine.addEntity(box1)
 const plane1 = new Entity()
 plane1.addComponent(new PlaneShape())
 plane1.getComponent(PlaneShape).withCollisions = false
-plane1.getComponent(PlaneShape).visible = false
+plane1.getComponent(PlaneShape).visible = true
 plane1.addComponent(pond)
 plane1.addComponent(new Transform({
-    position: new Vector3(32, .8, 160 ),
-    scale: new Vector3(316, 60, 1),
+    position: new Vector3(32, -10, 64 ),
+    scale: new Vector3(128, 60, 1),
     rotation: Quaternion.Euler(90, 90, 0)
     }))
 let triggerBox_music1 = new utils.TriggerBoxShape(
-  new Vector3 (54, 3.5, 320),
+  new Vector3 (54, 3.5, 120),
   new Vector3(0, 1, 0)
 );
 
@@ -60,12 +60,49 @@ onCameraExit : () => {
   pond.playing = false
 },
 
-enableDebug: false
+enableDebug: true
 }
 )
 )
 log('added new triggerbox')
 engine.addEntity(plane1)
+
+// Water
+const plane2 = new Entity()
+plane2.addComponent(new PlaneShape())
+plane2.getComponent(PlaneShape).withCollisions = false
+plane2.getComponent(PlaneShape).visible = true
+plane2.addComponent(pond)
+plane2.addComponent(new Transform({
+    position: new Vector3(32, -10, 256 ),
+    scale: new Vector3(128, 60, 1),
+    rotation: Quaternion.Euler(90, 90, 0)
+    }))
+let triggerBox_music2 = new utils.TriggerBoxShape(
+  new Vector3 (54, 3.5, 120),
+  new Vector3(0, 1, 0)
+);
+
+// Toggle audio on and off when entering / exiting the cube
+plane2.addComponent(
+new utils.TriggerComponent(
+triggerBox_music2,
+{
+onCameraEnter : () => {
+  log('play audio')
+  pond.playing = true
+},
+onCameraExit : () => {
+  log('stop audio')
+  pond.playing = false
+},
+
+enableDebug: true
+}
+)
+)
+log('added new triggerbox')
+engine.addEntity(plane2)
 
 
 }
